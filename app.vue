6c722da6aa299d9177c91fedbe4163cb8d5f2083
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2023-11-10 01:25:28 +0100

    feat: SEO and favicon

--- app.vue
+++ app.vue
@@ -1,3 +1,22 @@
+<script setup lang="ts">
+useHead({
+  title: 'Bernd Siegel Steingestaltung',
+  meta: [
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+  ],
+  link: [
+    { rel: 'icon', href: '/favicon.ico' },
+  ],
+  htmlAttrs: {
+    lang: 'de',
+  },
+})
+
+useSeoMeta({
+  description: 'Steingestaltung in Neudenau: Grabmale, Restaurierungen und mehr.'
+})
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtPage />
