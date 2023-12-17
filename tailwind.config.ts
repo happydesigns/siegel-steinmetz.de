c88965c1abceab40df1353ad83a70ee9ff274805
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2023-12-17 18:45:17 +0100

    feat: update colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,24 +1,25 @@
 import type { Config } from 'tailwindcss'
 
-export default <Partial<Config>> {
+export default <Partial<Config>>{
   content: [
     'docs/content/**/*.md',
   ],
   theme: {
     extend: {
+      // https://uicolors.app/create
       colors: {
         red: {
-          50: '#ffb8c7',
-          100: '#ff8aa3',
-          200: '#ff5c7f',
-          300: '#ff2e5b',
-          400: '#ff053b',
-          500: '#db0030',
-          600: '#b80028',
-          700: '#940020',
-          800: '#750019',
-          900: '#570013',
-          950: '#3d000d',
+          50: '#fef1f1',
+          100: '#fee6e6',
+          200: '#fccfd1',
+          300: '#f9a9aa',
+          400: '#f4767c',
+          500: '#eb4752',
+          600: '#d7283c',
+          700: '#b71a32',
+          800: '#9a1931',
+          900: '#631324',
+          950: '#490815',
         },
       },
     },
