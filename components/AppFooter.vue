789874f616494cb8d50b383e6f10385d6ee09513
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2024-01-01 05:00:26 +0100

    fix: color of theme select

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -6,7 +6,7 @@ const appConfig = useAppConfig()
 <template>
   <UFooter
     :ui="{
-      wrapper: 'bg-cool-800 dark:bg-cool-900 prose-invert dark',
+      wrapper: 'bg-cool-800 dark:bg-cool-900 dark',
       bottom: { wrapper: 'border-t border-gray-200 dark:border-cool-700', container: '!py-6', left: '~', center: '~', right: '~' },
       top: { wrapper: '', container: 'py-8 lg:py-12' },
     }"
@@ -15,8 +15,8 @@ const appConfig = useAppConfig()
       <UFooterColumns :links="appConfig.links?.footer">
         <template #right>
           <div class="space-y-4">
-            <UFormGroup name="theme" label="Farbschema" :ui="{ label: { base: 'font-serif font-semibold text-gray-900' } }">
-              <UColorModeSelect class="w-32 mt-6" select-class="dark:bg-cool-900" :ui="{ select: 'bg-slate-800' }" :ui-menu="{ background: 'dark:bg-cool-800', option: { active: 'dark:bg-cool-900' } }" />
+            <UFormGroup name="theme" label="Farbschema" class="light" :ui="{ label: { base: 'font-serif font-semibold text-gray-900' } }">
+              <UColorModeSelect class="w-32 mt-6" select-class="dark:bg-inherit" :ui="{ select: 'bg-slate-800' }" :ui-menu="{ background: 'dark:bg-cool-800', option: { active: 'dark:bg-cool-900' } }" />
             </UFormGroup>
           </div>
         </template>
