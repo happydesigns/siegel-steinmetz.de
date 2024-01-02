8b699c9a67ba19c42923eba60b42c60a61dcf3c6
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2024-01-02 03:35:49 +0100

    fix: wave covered by footer

--- components/content/Wave.vue
+++ components/content/Wave.vue
@@ -1,3 +1,3 @@
 <template>
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 720 17" class="-my-1"><path d="M0 10.5c89.5 6.315 201.941 9.256 338.75-.056C559.389-4.575 720 10.5 720 10.5V.38H0V10.5z" /></svg>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 720 17" class="-my-[1px]"><path d="M0 10.5c89.5 6.315 201.941 9.256 338.75-.056C559.389-4.575 720 10.5 720 10.5V.38H0V10.5z" /></svg>
 </template>
