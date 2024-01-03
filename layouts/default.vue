9a18172fc4ea35241c9c257240182b3dd54e9730
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2024-01-03 15:57:03 +0100

    fix: word overflow

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,7 +4,7 @@ const headline = computed(() => page?.value?._dir ? findPageHeadline(page?.value
 </script>
 
 <template>
-  <UMain :class="page?.ui?.wrapper">
+  <UMain :class="page?.ui?.wrapper" class="break-words">
     <UContainer :ui="{ padding: page?.container ? undefined : '', constrained: page?.container ? undefined : '' }">
       <UPageHeader v-if="page?.showHeader !== false" :title="page?.title" :description="page?.description" :links="page?.links" :headline="headline" />
       <UPageBody :prose="page?.prose !== false" class="pb-32" :class="[page?.ui?.body]">
