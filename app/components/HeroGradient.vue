944b091442c70fb419a6d7d84ea81053ae3eba68
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2024-12-12 20:58:37 +0100

    feat: overhaul landing page

---
+++ app/components/HeroGradient.vue
@@ -0,0 +1,14 @@
+<template>
+  <svg width="1628" height="714" viewBox="0 0 1628 714" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M32 497L275.715 664.554C283.716 670.055 293.198 673 302.908 673H1032.99C1047.54 673 1061.3 666.405 1070.41 655.067L1596 1" stroke="#770019" stroke-width="2" stroke-linecap="round" />
+    <path opacity="0.5" d="M16.4004 509.5L265.592 684.296C273.667 689.961 283.292 693 293.156 693H1042.98C1057.53 693 1071.3 686.399 1080.41 675.052L1611.6 13.5" stroke="#770019" stroke-width="2" stroke-linecap="round" />
+    <path opacity="0.25" d="M0.799805 522L255.478 704.049C263.622 709.87 273.381 713 283.392 713H1052.97C1067.53 713 1081.3 706.393 1090.41 695.039L1627.2 26" stroke="#770019" stroke-width="2" stroke-linecap="round" />
+    <path d="M275.715 664.554L32 497H1197.43L1070.41 655.067C1061.3 666.405 1047.54 673 1032.99 673H302.908C293.198 673 283.716 670.055 275.715 664.554Z" fill="url(#paint0_linear_1759_159)" fill-opacity="0.04" />
+    <defs>
+      <linearGradient id="paint0_linear_1759_159" x1="730.5" y1="673" x2="730.5" y2="507.5" gradientUnits="userSpaceOnUse">
+        <stop stop-color="#770019" />
+        <stop offset="1" stop-color="#DD002E" stop-opacity="0" />
+      </linearGradient>
+    </defs>
+  </svg>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -19,10 +19,10 @@ useSeoMeta({
     <ULandingSection
       v-bind="page.hero"
       :ui="{
-        wrapper: 'pt-6 sm:pt-6 md:pb-32 lg:py-32 2xl:py-36 overflow-x-hidden',
+        wrapper: 'pt-0 pb-28 sm:pt-6 md:pb-32 lg:py-32 2xl:py-36 overflow-hidden',
         headline: 'mb-8',
-        title: 'font-medium text-3xl md:text-4xl xl:text-5xl 2xl:text-6xl',
-        container: 'relative flex flex-col-reverse lg:grid !gap-8',
+        title: 'lg:font-medium text-3xl md:text-4xl xl:text-5xl 2xl:text-6xl',
+        container: 'relative overflow-visible flex flex-col-reverse lg:grid !gap-8',
       }"
     >
       <template v-if="page?.hero.headline" #headline>
@@ -37,31 +37,37 @@ useSeoMeta({
         </NuxtLink>
       </template>
 
-      <div class="lg:absolute md:right-0 lg:right-[-1rem] xl:right-[-8rem] 2xl:right-[-15rem]">
+      <HeroGradient class="lg:block absolute h-[768px] left-[-18rem] bottom-[-8rem] z-[-1] box-border" />
+
+      <div class="lg:absolute md:right-0 lg:right-[-1rem] xl:right-[-8rem] 2xl:right-[-15rem] -mx-4 sm:m-0">
         <div class="relative">
           <img
             :src="page.hero.image.src"
-            class="lg:block relative w-full lg:h-[360px] xl:h-[380px] 2xl:h-[420px] rounded-xl aspect-[16/9] sm:aspect-[18/9] md:aspect-[20/9] lg:aspect-[7/5] xl:aspect-[16/9] object-cover"
+            class="lg:block relative w-full lg:h-[360px] xl:h-[380px] 2xl:h-[420px] sm:rounded-xl aspect-[19/9] sm:aspect-[20/9] md:aspect-[21/9] lg:aspect-[7/5] xl:aspect-[16/9] object-cover md:object-[25%]"
             alt="NuxtHub Deploy page"
           >
-          <div class="hidden lg:block absolute inset-2 border border-4 dark:border-gray-900 rounded-md" />
+          <div class="hidden lg:block absolute inset-2 border border-4 border-gray-100 dark:border-gray-800 rounded-lg" />
         </div>
       </div>
     </ULandingSection>
 
     <!-- Landing Sections -->
-    <ULandingSection
-      v-for="(section, index) in page.sections"
-      :key="index"
-      v-bind="section"
-    >
-      <template #default>
-        <img
-          :src="section.image.src"
-          :alt="section.image.alt"
-          class="aspect-[10/7] object-cover"
-        >
-      </template>
-    </ULandingSection>
+    <!-- gradient from white to gray -->
+    <div class="bg-gradient-to-b from-white to-gray-100">
+      <ULandingSection
+        v-for="(section, index) in page.sections"
+        :key="index"
+        v-bind="section"
+      >
+        <template #default>
+          <img
+            :src="section.image.src"
+            :alt="section.image.alt"
+            :class="section.image.class"
+            class="aspect-[10/7] object-cover"
+          >
+        </template>
+      </ULandingSection>
+    </div>
   </div>
 </template>
