f4cbd2e2aac1bfd7908dd880d7cd3841b6b99e3a
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2024-12-14 01:14:23 +0100

    chore: use routeRules for prerendering

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -33,24 +33,24 @@ export default defineNuxtConfig({
     // '@nuxthq/studio',
     // '@nuxthub/core',
   ],
-  // routeRules: {
-  //   '/': { prerender: true },
-  //   'ueber-uns': { prerender: true },
-  //   'restaurierungen': { prerender: true },
-  //   'grabmale': { prerender: true },
-  //   'galerie': { prerender: true },
-  //   'kontakt': { prerender: true },
-  //   'impressum': { prerender: true },
-  //   'datenschutz': { prerender: true },
-  // },
-  nitro: {
-    prerender: {
-      // Pre-render the homepage
-      routes: ['/'],
-      // Then crawl all the links on the page
-      crawlLinks: true,
-    },
+  routeRules: {
+    '/': { prerender: true },
+    'ueber-uns': { prerender: true },
+    'restaurierungen': { prerender: true },
+    'grabmale': { prerender: true },
+    'galerie': { prerender: true },
+    'kontakt': { prerender: true },
+    'impressum': { prerender: true },
+    'datenschutz': { prerender: true },
   },
+  // nitro: {
+  //   prerender: {
+  //     // Pre-render the homepage
+  //     routes: ['/'],
+  //     // Then crawl all the links on the page
+  //     crawlLinks: true,
+  //   },
+  // },
   runtimeConfig: {
     public: { GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY },
   },
