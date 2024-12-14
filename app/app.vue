dfd71b7cfda3ae946319b716cefcb1a1c1593eb1
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2024-12-14 01:48:33 +0100

    chore: disable global styles, scripts

--- app/app.vue
+++ app/app.vue
@@ -1,22 +1,22 @@
 <script setup lang="ts">
-useHead({
-  title: 'Bernd Siegel Steingestaltung',
-  titleTemplate: '%s | Bernd Siegel Steingestaltung',
-  meta: [
-    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-    { name: 'google-site-verification', content: 'i5hJWTD7u44mb8sXv4PjgwP5EoPz0sWVuHd4rhbV6vk' },
-  ],
-  link: [
-    { rel: 'icon', href: '/favicon.ico' },
-  ],
-  htmlAttrs: {
-    lang: 'de',
-  },
-})
+// useHead({
+//   title: 'Bernd Siegel Steingestaltung',
+//   titleTemplate: '%s | Bernd Siegel Steingestaltung',
+//   meta: [
+//     { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+//     { name: 'google-site-verification', content: 'i5hJWTD7u44mb8sXv4PjgwP5EoPz0sWVuHd4rhbV6vk' },
+//   ],
+//   link: [
+//     { rel: 'icon', href: '/favicon.ico' },
+//   ],
+//   htmlAttrs: {
+//     lang: 'de',
+//   },
+// })
 
-useSeoMeta({
-  description: 'Steingestaltung in Neudenau: Grabmale, Restaurierungen und mehr.',
-})
+// useSeoMeta({
+//   description: 'Steingestaltung in Neudenau: Grabmale, Restaurierungen und mehr.',
+// })
 
 const { page } = useContent()
 </script>
@@ -28,7 +28,7 @@ const { page } = useContent()
 </template>
 
 <style>
-:root {
+/* :root {
   --color-primary-DEFAULT: var(--color-primary-800);
   --tw-prose-links: var(--color-primary-DEFAULT);
 }
@@ -52,5 +52,5 @@ img.default {
 
 .icon {
   @apply mb-0.5 mr-1;
-}
+} */
 </style>
