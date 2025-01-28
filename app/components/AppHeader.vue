8cc95de4b5437dc889b00d0496f38b4a744ead41
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2025-01-28 01:01:57 +0100

    refactor: update AppHeader for UI 3

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -3,19 +3,21 @@ const appConfig = useAppConfig()
 </script>
 
 <template>
-  <UHeader :links="appConfig.links.header">
-    <template #logo>
+  <UHeader>
+    <template #title>
       <Logo class="w-auto h-12 dark:bg-white rounded-md p-1" />
     </template>
 
-    <template #panel>
-      <UAsideLinks :links="appConfig.links.header" :ui="{ active: 'text-primary-900 dark:text-primary-400', icon: { active: 'bg-primary-900 dark:bg-primary', inactive: 'group-hover:bg-primary-900 dark:group-hover:bg-primary', wrapper: '~', base: '~' } }" />
-      <UDivider class="my-6" />
-      <UButton to="/kontakt" label="Kontakt" icon="i-ph-phone-duotone" block />
-    </template>
+    <UNavigationMenu :items="appConfig.links.header" />
 
     <template #right>
-      <UButton to="/kontakt" label="Kontakt" color="black" class="hidden lg:flex" icon="i-ph-phone-duotone" />
+      <UButton to="/kontakt" label="Kontakt" color="neutral" class="hidden lg:flex" icon="i-ph-phone-duotone" />
+    </template>
+
+    <template #content>
+      <UNavigationMenu :items="appConfig.links.header" orientation="vertical" :ui="{ active: 'text-primary-900 dark:text-primary-400', icon: { active: 'bg-primary-900 dark:bg-primary', inactive: 'group-hover:bg-primary-900 dark:group-hover:bg-primary', wrapper: '~', base: '~' } }" />
+      <USeparator class="my-6" />
+      <UButton to="/kontakt" label="Kontakt" icon="i-ph-phone-duotone" block />
     </template>
   </UHeader>
 </template>
