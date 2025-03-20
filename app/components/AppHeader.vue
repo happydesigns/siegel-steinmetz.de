8a2f65d2df50d09781df611fe13fac0964a142d4
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2025-03-21 00:27:15 +0100

    fix: use correct Header slot

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -11,13 +11,27 @@ const appConfig = useAppConfig()
     <UNavigationMenu :items="appConfig.links.header" />
 
     <template #right>
-      <UButton to="/kontakt" label="Kontakt" color="neutral" class="hidden lg:flex" icon="i-ph-phone-duotone" />
+      <UButton
+        to="/kontakt"
+        label="Kontakt"
+        color="neutral"
+        class="hidden lg:flex"
+        icon="i-ph-phone-duotone"
+      />
     </template>
 
-    <template #content>
-      <UNavigationMenu :items="appConfig.links.header" orientation="vertical" />
+    <template #body>
+      <UNavigationMenu
+        :items="appConfig.links.header"
+        orientation="vertical"
+      />
       <USeparator class="my-6" />
-      <UButton to="/kontakt" label="Kontakt" icon="i-ph-phone-duotone" block />
+      <UButton
+        to="/kontakt"
+        label="Kontakt"
+        icon="i-ph-phone-duotone"
+        block
+      />
     </template>
   </UHeader>
 </template>
