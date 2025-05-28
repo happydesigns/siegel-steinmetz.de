d57f372530a2f76853c3d57d128fd8ed60373688
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2025-05-29 01:00:18 +0200

    refactor: move layouts to ui-base

--- app/composables/usePageContent.ts
+++
@@ -1,6 +0,0 @@
-export function usePageContent(path?: string) {
-  const route = useRoute()
-  const key = path || route.path
-  const handler = () => queryCollection('content').path(key).first()
-  return useAsyncData(key, handler, { watch: [() => key] })
-}
